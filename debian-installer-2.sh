5b4e1aad0487ae3a1f35db870ab2e8b5fdf7082e
Ryan Waters <nikitis@gmail.com>
2011-07-04 14:36:07 -0400

    Cleaned up some more code

--- debian-installer-2.sh
+++ debian-installer-2.sh
@@ -18,18 +18,14 @@ export TERM=xterm-color
 echo " "
 echo "Setting up fstab entries. . ."
 echo " "
-sleep 3
 echo -e "/dev/ps3dd2	/		extextvar	defaults		0 1\n/dev/ps3vram	none		swap	sw			0 0\n/dev/ps3dd1	none		swap	sw			0 0\n/dev/sr0	/mnt/cdrom	auto	noauto,ro		0 0\nproc		/proc		proc	defaults		0 0\nshm		/dev/shm	tmpfs	nodev,nosuid,noexec	0 0\n" > /etc/fstab
 
 
 ## Setting up timezone
 
-echo " "
 echo "Setting up timezone data"
 echo " "
-sleep 3
 touch /etc/default/rcS
-
 dpkg-reconfigure tzdata
 
 
@@ -51,8 +47,7 @@ echo -e "auto lo\niface lo inet loopback\n\nauto eth0\niface eth0 inet dhcp\n" >
 
 ## Setting up /etc/resolv.conf
 
-echo " "
-read -p "If you have a different gateway other than 192.168.1.1, please enter it in. Otherwise press n: " E
+read -p "If you have a different nameserver other than 192.168.1.1, please enter it now, or press (n): " E
 if [ "$E" = n ]; then
 	echo "nameserver 192.168.1.1" > /etc/resolv.conf
 else
@@ -63,9 +58,8 @@ echo " "
 ## Configuring aptitude sources in /etc/apt/sources.list
 
 echo " "
-echo "Creating entries for aptitude."
+echo "Creating entries for sources.list"
 echo " "
-sleep 3
 echo -e "deb http://ftp.us.debian.org/debian squeeze main\ndeb-src http://ftp.us.debian.org/debian squeeze main\n\ndeb http://security.debian.org/ squeeze/updates main\ndeb-src http://security.debian.org/ squeeze/updates main\n" > /etc/apt/sources.list
 
 
@@ -76,13 +70,12 @@ echo "Updating base install package index."
 echo " "
 aptitude update
 echo " "
-echo "Setting up locales and console-data.  For english set en-us."
+echo "Setting up locales and console-data.  For english set en-us-UTF8."
 echo " "
-sleep 5
 
-aptitude install locales
+aptitude -y install locales
 dpkg-reconfigure locales
-aptitude install console-data
+aptitude -y install console-data
 dpkg-reconfigure console-data
 
 
@@ -91,7 +84,7 @@ dpkg-reconfigure console-data
 echo " "
 echo "Installing other packages that are needed."
 echo " "
-sleep 5
+sleep 2
 
 echo " "
 echo "Starting tasksel. . ."
@@ -100,13 +93,28 @@ sleep 3
 tasksel install standard
 echo "Cleaning up install packages to save space on HDD. . ."
 aptitude clean
+
+
+## User creation and password setting
+echo "Starting user creation and password entries..."
+
 echo "Please set a new root password."
 passwd
+echo " "
+
+read -p "Please enter in a username you would like to use: " F
+if [ "$F" = "" ]; then
+	echo "That username was not valid"
+else
+	echo "Creating user $F"
+	adduser $F
+fi 
+
 
 echo " "
 echo "Installing development packages for kernel build"
 echo " "
-aptitude install git build-essential ncurses-dev
+aptitude -y install git build-essential ncurses-dev
 
 
 ## Creating Swap Parition and Enabling
@@ -117,17 +125,28 @@ echo " "
 mkswap /dev/ps3dd1
 swapon /dev/ps3dd1
 
-## Kernal compiling (Still very beta)
 
+## Git cloning of Kernal)
+echo "Beginning clone of kernel git. (May take a couple of hours.)"
 git clone --depth 1 git://git.gitbrew.org/ps3/ps3linux/linux-2.6.git /usr/src/linux-2.6
 ln -sf /usr/src/linux-2.6 /usr/src/linux
 cp /usr/src/linux/ps3_linux_config /usr/src/linux/.config
+
+
+## Kernel compilation
+
+echo " "
+echo "Starting compilation of kernel. (Takes around 1 hour or less.)"
 cd /usr/src/linux
 make menuconfig
 make
 make install
 make modules_install
 cd /
+echo " "
+echo "Kernel compiling is done if no errors occured."
+echo " "
+
 
 ## Creating kboot.conf entry
 
@@ -151,9 +170,10 @@ echo -e "KERNEL==\"ps3vflash\", SYMLINK+=\"ps3flash\"" > /etc/udev/rules.d/70-pe
 ## Finished
 
 echo " "
-echo "Installation is complete."
+echo "Installation is complete. Upon reboot, select your new kboot entry to boot Debian."
 echo " "
-echo "Type Reboot.  Next time you are in petitboot, select your new entry to boot debian."
+read -p "Press any key to reboot.  (If system hangs, hold power button for 8 seconds.)"
+
 echo " " 
-echo "Enjoy!  This installer was written by nikitis from Gitbrew."
+echo "Enjoy!"
 
