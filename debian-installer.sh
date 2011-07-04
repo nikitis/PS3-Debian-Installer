c6b9aa60a6bc1f66737e264c1bc6ce77949fef28
Ryan Waters <nikitis@gmail.com>
2011-07-04 14:35:44 -0400

    Added beginning partitioner

--- debian-installer.sh
+++ debian-installer.sh
@@ -4,29 +4,108 @@
 
 ## Please note that this is a work in progress
 
-## Version 0.89
+## Version 0.95
+
+echo "PPPPPP    SSSSSSS   3333333"
+echo "PP   PP   SS             33"
+echo "PPPPPP    SSSSSSS   3333333"
+echo "PP             SS        33"
+echo "PP        SSSSSSS   3333333"
+
+echo "DDDDDD    EEEEEEE   BBBBBB    IIIIIIII      AA      NN    NN"
+echo "DD   DD   EE        BB   BB      II       AA  AA    NNNN  NN"
+echo "DD   DD   EEEEE     BBBBBB       II      AAAAAAAA   NN NN NN"
+echo "DD   DD   EE        BB   BB      II      AA    AA   NN  NNNN"
+echo "DDDDDD    EEEEEEE   BBBBBB    IIIIIIII   AA    AA   NN    NN"
+echo " "
+echo "Written By: nikitis"
 
 echo " "
-echo "I am in testing phase.  Just so you know.  I may not fully work yet.  Sorry"
+echo "Installer is in testing phase.  You may experience issues, but should not brick"
+echo "your console as it only works within the /dev/ps3dd environment."
 echo " "
-echo "Currently this script assumes that you have a /dev/ps3dd1 dedicated for swap,"
-echo "and /dev/ps3dd2 dedicated for root (/) If this is not the case, please hit"
-echo "ctrl-c now to cancel this script as it will not work correctly for the moment."
+echo "If you have not yet created the /dev/ps3dd region by running the create_hdd_region.sh"
+echo "script, please hit ctrl-c now and do this first before running this script"
 echo " "
-echo "Waiting 15 seconds before continuing. . ."
-sleep 15
+echo "This installer will create a /dev/ps3dd1 dedicated for swap,"
+echo "and /dev/ps3dd2 dedicated for root (/). If you have already created a"
+echo "partitioning scheme this script will attempt to remove them and setup the default"
+echo "scheme listed above.  In furture releases I will provide choice of schema."
+echo " "
+echo "Please hit ctrl-c now to cancel this script if this does not work for you."
+echo " "
+read -p "Please press enter to continue."
 
 
+## Setting up device variable
+
+DEVICE="/dev/ps3dd"
+
+
+## parted commands
+read -p "Have you already created partitions on your PS3 for Linux? (y/n): " G
+if [ "$G" = n ]; then
+	parted ${DEVICE} --script -- mklabel GPT
+	parted ${DEVICE} --script -- mkpart primary 0 2GB
+	parted ${DEVICE} --script -- mkpart primary 2GB -1
+	parted ${DEVICE} --script -- print
+elif [ "$G" = y ]; then
+
+	read -p "How many partitions were created? " H
+	if [ "$H" = 1 ]; then
+		parted ${DEVICE} --script -- rm 1
+		parted ${DEVICE} --script -- mklabel GPT
+		parted ${DEVICE} --script -- mkpart primary 0 2GB
+		parted ${DEVICE} --script -- mkpart primary 2GB -1
+		parted ${DEVICE} --script -- print
+	elif [ "$H" = 2 ]; then 
+		parted ${DEVICE} --script -- rm 2
+		parted ${DEVICE} --script -- rm 1
+		parted ${DEVICE} --script -- mklabel GPT
+		parted ${DEVICE} --script -- mkpart primary 0 2GB
+		parted ${DEVICE} --script -- mkpart primary 2GB -1
+		parted ${DEVICE} --script -- print
+	elif [ "$H" >= 3 ]; then
+		echo "If you have 3 or more partitions this installer will not currently work for you unless"
+		echo "you are willing to wipe them and start over."
+		echo " "
+		read -p "To continue press enter.  To quit press (x): " X
+		if [ "$X" = x ]; then
+			killall debian-installer.sh
+		else
+			echo " "
+			echo "Attempting to remove all partitions created for ps3dd.  Ignore any errors."
+			parted ${DEVICE} --script -- rm 5
+			parted ${DEVICE} --script -- rm 4
+			parted ${DEVICE} --script -- rm 3
+			parted ${DEVICE} --script -- rm 2
+			parted ${DEVICE} --script -- rm 1
+			parted ${DEVICE} --script -- mklabel GPT
+			parted ${DEVICE} --script -- mkpart primary 0 2GB
+			parted ${DEVICE} --script -- mkpart primary 2GB -1
+			parted ${DEVICE} --script -- print
+		fi
+	else
+		read -p "You have failed to enter a proper number and have crashed the installer.  Please reboot and start over."
+		reboot
+	fi
+else
+	echo "You have failed to enter in a (y) or (n).  Please try again, rebooting..."
+	reboot
+fi
+
+ 
 ## Unmounts the partition
-
+echo "Attempting to unmount the partition /dev/ps3dd2. If it errors it should be okay."
 echo "Unmounting partition /dev/ps3dd2"
 umount /dev/ps3dd2
+echo " "
 
 
 ## Creates the directory to chroot to.
-
+echo "Creating chroot directory /mnt/debian"
 mkdir /mnt/debian
-
+echo " "
 
 ## Choice of type of formatting to be used on ps3dd2
 read -p "Which filesystem type do you wish "root" to have?  ext(2/3/4) (s)kip: " A
@@ -67,20 +146,29 @@ fi
 
 
 ## Mounts / to /mnt/debian/
-
+echo " "
+echo "Mounting /dev/ps3dd2 to the chroot dir."
 mount /dev/ps3dd2 /mnt/debian
 
 
 ## This step is used for when the script is re-run 
-
-echo "Cleaning formatted drive. . ."
+echo " "
+echo "Cleaning formatted drive (In case there was previous install attempt.)."
 rm -rf /mnt/debian/*
+echo " "
+
+
+## Setting tcp_enc to 0
+echo "Disabling tcp_enc for older petitboot installs to fix debootstrap"
+echo "0" > /proc/sys/net/ipv4/tcp_enc
+echo " "
 
 
 ## Debootstrap
 
-echo "Debootstrapping. . ."
+echo "Debootstrapping... This process can take a couple of minutes."
 debootstrap --arch powerpc squeeze /mnt/debian http://ftp.us.debian.org/debian
+echo " "
 
 echo "Copying 2nd half of installer and variables over to post-chrooted environment"
 cp ./debian-installer-2.sh /mnt/debian/tmp/debian-installer-2.sh
